d28038e378536c18763c822292f211319f46c5ce
MoisesR-coder <moises@ivitec.mx>
2025-08-27 20:18:49 -0400

    d

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       '/api': {
         target: 'http://31.220.98.150:8000',
         changeOrigin: true,
-        rewrite: (path) => path.replace(/^/api/, '')
+        rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
   }
